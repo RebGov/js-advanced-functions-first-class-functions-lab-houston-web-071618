f3de16ed430db42fb4166e159754ee928b413f04
RebGov <becci.govert@gmail.com>
2018-08-27 17:14:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers= function (collection) {
+  return newCollection = collection.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(collection) {
+  return collection.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function (valueMultiplyBy) {
+  return function (value) {
+    return valueMultiplyBy * value;
+  };
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+const selectDifferentDrivers = function (collection, whichOnes) {
+  return whichOnes(collection);
+}
